b5f20beb09e7a81afbf1ff51a394b5c15a1e8486
Rodolfo Rosa Galdino da Silva <rodolforosasilva@gmail.com>
2019-03-04 00:04:07 -0300

    iniciando modulo 3 do goreact - ate a aula de reactotron

---
+++ goreact/modulo3/app3/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Provider } from "react-redux";
+
+import "./config/reactotron";
+import store from "./store";
+
+import TodoList from "./TodoList";
+
+const App = () => (
+  <Provider store={store}>
+    <TodoList />
+  </Provider>
+);
+
+export default App;
