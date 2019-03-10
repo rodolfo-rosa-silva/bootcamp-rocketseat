81305feed19307b1264ae131ddc49ecc3170200b
Rodolfo Rosa Galdino da Silva <rodolforosasilva@gmail.com>
2019-03-09 23:49:18 -0300

    modulo 2 do gonative completo - faltando apenas o desafio

--- gonative/modulo2/src/pages/Repositories/index.js
+++ gonative/modulo2/src/pages/Repositories/index.js
@@ -1,9 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import { View } from 'react-native';
+import {
+  View, AsyncStorage, ActivityIndicator, FlatList,
+} from 'react-native';
 
-// import styles from './styles';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import api from '../../services/api';
 
-const Repositories = () => <View />;
+import Header from '../../components/Header';
 
-export default Repositories;
+import RepositoryItem from './RepositoryItem';
+import styles from './styles';
+
+const TabIcon = ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />;
+
+TabIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired,
+};
+
+export default class Repositories extends Component {
+  static navigationOptions = {
+    tabBarIcon: TabIcon,
+  };
+
+  state = {
+    data: [],
+    loading: true,
+    refreshing: false,
+  };
+
+  componentDidMount() {
+    this.loadRepositories();
+  }
+
+  loadRepositories = async () => {
+    this.setState({ refreshing: true });
+
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const { data } = await api.get(`users/${username}/repos`);
+
+    this.setState({ data, loading: false, refreshing: false });
+  };
+
+  renderListItem = ({ item }) => <RepositoryItem respository={item} />;
+
+  renderList = () => {
+    const { data, refreshing } = this.state;
+
+    return (
+      <FlatList
+        data={data}
+        keyExtractor={item => String(item.id)}
+        renderItem={this.renderListItem}
+        onRefresh={this.loadRepositories}
+        refreshing={refreshing}
+      />
+    );
+  };
+
+  render() {
+    const { loading } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <Header title="Repositorios" />
+        {loading ? <ActivityIndicator style={styles.loading} /> : this.renderList()}
+      </View>
+    );
+  }
+}
--- gonative/modulo2/src/pages/Welcome/index.js
+++ gonative/modulo2/src/pages/Welcome/index.js
@@ -1,33 +1,90 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import {
-  View, Text, TextInput, TouchableOpacity, StatusBar,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StatusBar,
+  AsyncStorage,
+  ActivityIndicator,
 } from 'react-native';
+import api from '../../services/api';
 
 import styles from './styles';
 
-const Welcome = () => (
-  <View style={styles.container}>
-    <StatusBar barStyle="light-content" />
-    <Text style={styles.title}>Bem vindo</Text>
-    <Text style={styles.text}>
-      Para continuar precisamos que voce informe seu usuario no GitHub.
-    </Text>
+export default class Welcome extends Component {
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
 
-    <View style={styles.form}>
-      <TextInput
-        style={styles.input}
-        autoCapitalize="none"
-        autoCorrect={false}
-        placeholder="Digite seu usuario"
-        underlineColorAndroid="transparent"
-      />
-    </View>
+  state = {
+    username: '',
+    loading: false,
+    error: false,
+  };
 
-    <TouchableOpacity style={styles.button} onPress={() => {}}>
-      <Text style={styles.buttonText}>Prosseguir</Text>
-    </TouchableOpacity>
-  </View>
-);
+  checkUserExists = async (username) => {
+    const user = await api.get(`/users/${username}`);
 
-export default Welcome;
+    return user;
+  };
+
+  saveUser = async (username) => {
+    await AsyncStorage.setItem('@Githuber:username', username);
+  };
+
+  signIn = async () => {
+    const { username } = this.state;
+    const { navigation } = this.props;
+
+    this.setState({ loading: true });
+
+    try {
+      await this.checkUserExists(username);
+      await this.saveUser(username);
+
+      navigation.navigate('User');
+    } catch (err) {
+      this.setState({ loading: false, error: true });
+    }
+  };
+
+  render() {
+    const { username, loading, error } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>Bem vindo</Text>
+        <Text style={styles.text}>
+          Para continuar precisamos que voce informe seu usuario no GitHub.
+        </Text>
+
+        {error && <Text style={styles.error}>Usuario inexistente.</Text>}
+
+        <View style={styles.form}>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Digite seu usuario"
+            underlineColorAndroid="transparent"
+            value={username}
+            onChangeText={text => this.setState({ username: text })}
+          />
+        </View>
+
+        <TouchableOpacity style={styles.button} onPress={this.signIn}>
+          {loading ? (
+            <ActivityIndicator size="small" color="#FFF" />
+          ) : (
+            <Text style={styles.buttonText}>Prosseguir</Text>
+          )}
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
